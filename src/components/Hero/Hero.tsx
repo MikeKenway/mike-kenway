a87c0689b3dd6e66ddbf5d7191d5e5a45971402c
Mike Kenway <mike@mikekenway.com>
2025-05-21 00:21:16 -0400

    Added Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,34 +1,51 @@
+// Hero.tsx
+
 import styles from './Hero.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className={styles.hero}>
-      <div className='container'>
-        <h1 className={styles.title}>
-          I design scalable systems and thoughtful experiences <br />
-          that drive clarity, growth, and momentum.
-        </h1>
+    <section className={styles.heroWrap}>
+      <div className={styles.heroGrid}>
+        {/* Left Grid Column – Text */}
+        <div className={styles.heroItem}>
+          <h1 className={styles.title}>Hi! I'm Kenway.</h1>
 
-        <p className={styles.subtitle}>
-          UX/UI Designer • Creative Director • Founder <br />
-          Helping startups, artists, and indie brands make smarter digital
-          decisions—without the agency fluff.
-        </p>
+          <p className={styles.subtitle}>
+            UX/UI Designer • Creative Director • Founder
+          </p>
+          <p className={styles.heroText}>
+            I design scalable systems and thoughtful experiences <br />
+            that drive clarity, growth, and momentum.
+          </p>
+          <div className={styles.actions}>
+            <Link
+              href='/cases'
+              className='primaryBtn'
+            >
+              See My Work
+            </Link>
+            <Link
+              href='/contact'
+              className='secondaryBtn'
+            >
+              Let’s Talk
+            </Link>
+          </div>
+        </div>
 
-        <div className={styles.actions}>
-          <Link
-            href='/cases'
-            className='primaryBtn'
-          >
-            See My Work
-          </Link>
-          <Link
-            href='/contact'
-            className={styles.secondaryBtn}
-          >
-            Let’s Talk
-          </Link>
+        {/* Right Grid Column – Image */}
+        <div className={styles.heroImg}>
+          <div className='image-cover'>
+            <Image
+              src='/images/kenway.jpg'
+              alt='headshot photo of Mike Kenway'
+              width={1000}
+              height={1000}
+              priority
+            />
+          </div>
         </div>
       </div>
     </section>
